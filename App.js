00079ee8f49a2a6ec39b3ad41af565b21d8876d0
Ocloo Priscilla <ocloopriscy98@gmail.com>
2021-09-16 01:06:40 +0000

    online shopping app initialized

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import {StatusBar} from 'expo-status-bar';
+import React  from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AddToCartScreen from './src/screens/AddToCartScreen';
+import OnlineShoppingScreen from './src/screens/OnlineShoppingScreen';
+import PaymentSuccessfulScreen from './src/screens/PaymentSuccesfulScreen';
+
+
+import {createStackNavigator,  } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+       <NavigationContainer>
+         <Stack.Navigator
+           screenOption  = {{
+               header: () => null,
+           }} 
+         >
+           <Stack.Screen name="OnlineScreen" component= {OnlineShoppingScreen} ></Stack.Screen>
+           <Stack.Screen name="CartScreen" component= {AddToCartScreen}></Stack.Screen>
+           <Stack.Screen name="PaymentScreen" component= {PaymentSuccessfulScreen}></Stack.Screen>
 
+         </Stack.Navigator>
+
+
+            
+     </NavigationContainer>
+               
+
+      
+
+
+
+        
+    )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+
+  
+})
